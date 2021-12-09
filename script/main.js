e045cc3b0a0b5b190cf0f5d795e914ad1c685460
Nam Xuan Nguyen <nam.xuan.nguyen@est.tech>
2021-12-09 21:24:33 +0200

    Fix

--- script/main.js
+++ script/main.js
@@ -103,16 +103,16 @@ const animationTimeline = () => {
     )
     .from(".idea-1", 0.7, ideaTextTrans)
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
+    .from(".idea-2", 1, ideaTextTrans)
+    .to(".idea-2", 1, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 1, ideaTextTrans)
+    .to(".idea-3 strong", 1, {
       scale: 1.2,
       x: 10,
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff"
     })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=2.5")
+    .to(".idea-3", 1, ideaTextTransLeave, "+=1.5")
     .from(".idea-4", 0.7, ideaTextTrans)
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
     .from(
